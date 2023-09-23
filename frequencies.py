eaa8316e3aca46dfb07cd2f7df0106fe258d4b2e
Marin Shpata <marin.shpata@kcl.ac.uk>
2023-09-23 17:57:24 +0100

    Solution

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,11 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    for i in items:
+        if str(i) not in frequencies:
+            frequencies[str(i)] = 1
+        else:
+            frequencies[str(i)] +=1
     return frequencies
+
+print(frequencies([100, 'Hello', '100', '100', 100]))
